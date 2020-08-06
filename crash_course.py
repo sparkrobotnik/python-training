8c6818f371b089aed1112ab8a2bc78fb5655d34c
sparkrobotnik <sparkrobotnik@gmail.com>
2020-08-06 17:16:06 -0300

    ADICIONANDO ARQUIVO INICIAL

---
+++ crash_course.py
@@ -0,0 +1,10 @@
+usuario = {
+	'nome':	input("informe seu nome(completo): "),
+	'idade':input("qual a sua idade: "),
+	'email':input("email: ")
+}
+
+print("———————————————————————————————————————————————")
+print(f"seu nome é {usuario['nome']}")
+print(f"você tem {usuario['idade']} anos")
+print(f"seu email é {usuario['email']}")
