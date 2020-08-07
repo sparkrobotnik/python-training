d5fbe40a8ab8b07449101d7366bd365abe9bd603
sparkrobotnik <sparkrobotnik@gmail.com>
2020-08-07 00:56:06 -0300

    ADICOES NA LOGICA DO APP

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from custom_utils import clear
 from time import sleep
 
+#MOSTRAR INFORMACOES DO APP
 clear()
 app_config = {
 	'nome':		"library assistant",
 	'versao': "1.0",
 	'autor':	"spark robotnik"
 }
+print(f"{app_config['nome']} versão: {app_config['versao']} - autor: {app_config['autor']}")
+
+#PEDIR CREDENCIAIS DO USUARIO
+resposta = "";
+sleep(2)
+clear()
 usuario = {
 	'nome':	input("informe seu nome(completo): "),
 	'idade':input("qual a sua idade: "),
 	'email':input("email: ")
 }
 
+#PEDIR CONFIRMAÇÃO DOS DADOS
 clear()
 print(f"NOME: 	{usuario['nome']}")
 print(f"IDADE:	{usuario['idade']} anos")
 print(f"EMAIL:	{usuario['email']}")
+
+resposta = input("\n\tESTE DADOS ESTÃO CORRETOS? (S / N): ")
