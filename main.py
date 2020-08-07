33625ba69864aadfd013194de25f0651958d5037
sparkrobotnik <sparkrobotnik@gmail.com>
2020-08-07 14:00:55 -0300

    ATUALIZACAO DE LOGICA DO APP

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from custom_utils import clear
 from time import sleep
 
-#MOSTRAR INFORMACOES DO APP
-clear()
 app_config = {
 	'nome':		"library assistant",
 	'versao': "1.0",
 	'autor':	"spark robotnik"
 }
+usuario = {
+	'nome':		"",
+	'idade':	0,
+	'email':	""
+}
+
+#MOSTRAR INFORMACOES DO APP
+clear()
 print(f"{app_config['nome']} versão: {app_config['versao']} - autor: {app_config['autor']}")
 
 #PEDIR CREDENCIAIS DO USUARIO
-resposta = "";
 sleep(2)
-clear()
-usuario = {
-	'nome':	input("informe seu nome(completo): "),
-	'idade':input("qual a sua idade: "),
-	'email':input("email: ")
-}
 
-#PEDIR CONFIRMAÇÃO DOS DADOS
-clear()
-print(f"NOME: 	{usuario['nome']}")
-print(f"IDADE:	{usuario['idade']} anos")
-print(f"EMAIL:	{usuario['email']}")
-
-resposta = input("\n\tESTE DADOS ESTÃO CORRETOS? (S / N): ").upper()
+resposta = "";
+while resposta != "S":
+	resposta = ""
+	
+	clear()
+	
+	print("\t———CADASTRO DE USUARIO———\n")
+	usuario['nome']	= input("NOME(COMPLETO): ")
+	usuario['idade']= input("IDADE:\t\t")
+	usuario['email']= input("EMAIL:\t\t")
+	
+	#PEDIR CONFIRMAÇÃO DOS DADOS
+	while resposta != "S" and resposta != "N":
+		clear()
+		
+		print("\t———CONFIRMAÇÃO DE DADOS———\n")
+		print(f"NOME: 	{usuario['nome']}")
+		print(f"IDADE:	{usuario['idade']} anos")
+		print(f"EMAIL:	{usuario['email']}")
+		
+		resposta = input("\n\tESTE DADOS ESTÃO CORRETOS? (S / N): ").upper()
